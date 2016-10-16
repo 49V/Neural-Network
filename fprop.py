e6f49ad930528215edafee998a1cfd66a4d706af
moshelawlor@gmail.com <moshelawlor@gmail.com>
2016-10-15 22:00:05 -0700

    This version has the untested gradient descent algorithm. One thing that must be completed before testing is creating our code as a class. It is much easier to work with this way and this will be attempted before verifying the operation of gradient descent. The nice thing is that there are no Python errors with regards to the code. I have also added a "Neural Networks" text file for reference.

--- fprop.py
+++ fprop.py
@@ -1,5 +1,3 @@
-#Import our boy numpy
-import numpy as np
 import math
 import random
 from pprint import pprint
@@ -8,6 +6,13 @@ def array (length, width):
 	"""Creates an array of zeroes with specified dimensions"""
 	return [[0 for i in range(width)] for j in range(length)] #List comprehensions (Works like two for loops)
 
+def matrixElementMultiply (value, list):
+	"""Multiplies each element of a list by the given value"""
+	numRows = len(list)
+	numCols = len(list[0])
+	
+	return [[value * list[j][i] for i in range(numCols)] for j in range(numRows)]
+	
 #Use double loops to create a random array of neat stuff
 def randomArray (length, width):
 	"""Creates a random array of dimensions as per length and width"""
@@ -147,15 +152,30 @@ def fprop(input, netSize):
 
 #For test purposes just use netSize
 def bprop(a, z, weights, biases,netSize):
+	"""
+	Compute the gradients for the biases and the weights for a given input and ouput tuple (x, y)
+	
+	a       : Matrix of Activations
+	z       : Matrix of Weighted Inputs
+	weights : Matrix containing all weights
+	biases  : Matrix containing all biases
+	netSize : List containing sizing dimensions of a network i.e. (2, 3, 1) means a networks that
+	has 3 layers. 2 neurons in the input layer, 3 neurons in the hidden layer, and 1 neuron in the 
+	output layer.
+	"""
 
+	"""
+	1) Initialization
+	"""
 	outputDesired = [[0.5]]
 	#Initialize delta
 	delta = [array(i, 1) for i in netSize]
 	print "Delta:"
 	pprint(delta)
 	
-
-	#DELTA BABY
+	"""
+	2) Compute output error, delta
+	"""
 	delta[-1] = hadmardProduct((matrixSubtraction(a[-1], outputDesired)), sigmoidPrimeArray(z[-1]))
 	print "Delta - 1"
 	pprint(delta)
@@ -163,10 +183,61 @@ def bprop(a, z, weights, biases,netSize):
 	#We want to go from the second last value until the second value!
 	interval = range(len(netSize) - 2, 0, -1)
 	
+	"""
+	3) Backpropagate the error to all previous layers.
+	"""
 	#Now it's time to backpropagate
 	#Remember, only weights for in between each layer!
 	delta[i] = [hadmardProduct(matrixMultiply(weights[i], delta[i + 1]), sigmoidPrimeArray(z[i])) for i in interval]
 	
+	"""
+	4) Compute Gradients.
+	"""
+	#We have weights for every layer except for the last
+	numLayers = len(netSize[0])
+	gradient_weights[i] =[matrixMultiply(delta[i], transpose(a[-i - 1])) for i in range(2, numLayers)] 
+	#We have biases for every layer but the first, and the last layer
+	gradient_biases = delta
+	
+	
 	print "\n DELTA FINAL: \n"
 	pprint(delta)
 	
+	print "\n GRADIENT WEIGHTS: \n"
+	pprint(gradient_weights)
+	
+	print "\n GRADIENT BIASES: \n"
+	pprint(gradient_biases)
+	
+	
+	return gradient_biases, gradient_weights
+	
+
+def gradientDescent(eta, miniBatch, netSize):
+	"""
+	#Takes a learning rate eta, and a mini batch (miniBatch) and returns updated biases and weights
+	#based on gradients.
+	"""
+	batchSize = len(miniBatch)
+	
+	#REALLY INEFFICIENT, NEED TO USE LESS LISTS! SO MANY RESOURCES MOSHE! Alright for POC
+	sum_biases  = [array(i,1) for i in netSize[1:]]
+	temp_biases = [array(i,1) for i in netSize[1:]]
+	eta_biases  = [array(i,1) for i in netSize[1:]] 
+	
+	sum_weights  = [[array(i,j) for i,j in zip(netSize[:-1], netSize[1:])]]
+	temp_weights = [[array(i,j) for i,j in zip(netSize[:-1], netSize[1:])]]
+	eta_weights  = [[array(i,j) for i,j in zip(netSize[:-1], netSize[1:])]]
+	
+	#First calculate the sum of the gradients of the biases, and weights respectively
+	for x,y in miniBatch:
+		temp_biases, temp_weights = bprop(x, y, netSize)
+		#Remember you have lists within lists, can't just add.
+		sum_biases  = [matrixAddition(i, j) for i, j in zip(sum_biases, temp_biases)]
+		sum_weights = [matrixAddition(i, j) for i, j in zip(sum_weights, temp_weights)]
+
+	#Then update the biases and weights using the learning rate and batch size
+	biases  = [matrixSubtraction(b, ((eta/batchSize) * sb)) for b, sb in zip(biases, sum_biases)]
+	weights = [matrixSubtraction(w, ((eta/batchSize) * sw)) for w, sw in zip(weights, sum_weights)]
+	
+	return biases, weights
